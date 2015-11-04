488e376b392571f683f8b4d0272c68c9444181eb
trevor-carpenter <trevor.carpenter@sams.ac.uk>
2015-11-04 17:06:59 +0000

    debugged rk4.m
    %TODO check transpose yOfx

--- BrickmanModelFunctions/euler.m
+++ BrickmanModelFunctions/euler.m
@@ -2,6 +2,7 @@ function [ T,Y ] = euler( ode,t,h,y,options)
 %EULER Summary of this function goes here
 %   Detailed explanation goes here
 a=ode(t,y);
+%TODO annoying transpose - I am doing something wrong
 Y=y'+h*a;
 T=t+h;
 end
--- BrickmanModelFunctions/rk4.m
+++ BrickmanModelFunctions/rk4.m
@@ -3,10 +3,14 @@ function [ T,Y ] = rk4( ode,t,h,yOfx,options)
 %   Detailed explanation goes here
 ho2=h/2;
 a=ode(t,yOfx); %a is in the space of dy(x)/dx
-b=ode(t+ho2,yOfx+ho2*a); %then ho2*a is in the space y(x) 
-c=ode(t+ho2,yOfx+ho2*b);
-d=ode(t+h,yOfx+h*c);
-Y=yOfX+h*(a+2*b+2*c+d)/6;
+%TODO annoying transpose - I am doing something wrong
+b=ode(t+ho2,yOfx'+ho2*a); %then ho2*a is in the space y(x) 
+%TODO annoying transpose - I am doing something wrong
+c=ode(t+ho2,yOfx'+ho2*b);
+%TODO annoying transpose - I am doing something wrong
+d=ode(t+h,yOfx'+h*c);
+%TODO annoying transpose - I am doing something wrong
+Y=yOfx'+h*(a+2*b+2*c+d)/6;
 T=t+h;
 end
 
