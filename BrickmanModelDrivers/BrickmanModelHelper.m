f712ac893134ad95ca9b1af4a53f833f7505b495
trevor-carpenter <trevor.carpenter@sams.ac.uk>
2015-11-05 10:47:07 +0000

    Applied function handle
    addressed TODO use function handle and removed duplicate code in switch
    statement in BrickmanModelHelper.m
    
    Wrote doc comments in BrickmanModelHelper.m

--- BrickmanModelDrivers/BrickmanModelHelper.m
+++ BrickmanModelDrivers/BrickmanModelHelper.m
@@ -1,7 +1,30 @@
 function [ brickmanModel ] = BrickmanModelHelper(dx,L,dy,W,dt,T,startX,startY,x0,R,u0,method,options)
-%BRICKMANMODELHELPER Summary of this function goes here
-%   Detailed explanation goes here
-%contain
+%BRICKMANMODELHELPER runs Brickman penisula model
+%   [ brickmanModel ] = BrickmanModelHelper(dx,L,dy,W,dt,T,startX,startY,x0,R,u0,method,options)
+%   Run the Brickman model with the parameters and the output retuned in a
+%   struct. The input parameters are as follows:
+%       dx      aGrid element side length (m)
+%       L       model domain length (m)
+%       dy      aGrid element side length (m)
+%       W       model domain width (m)
+%       dt      time step size (s)
+%       T       total time to simulate (s)
+%       startX  start position x coordinate applied to all particles (m)
+%       startY  array of start position y coordinates (m)
+%       x0      centre of penisula (m)
+%       R       radius of peninsula (m)
+%       u0      initial velocity of particles t=0
+%       method  string containing name of solution method
+%       options used by some solution methods to control step size
+%   The input parameters and the results are returned in a struct, in 
+%   particular: 
+%   brickmanModel.aGridSamples      see brickmanAGrid for details
+%   brickmanModel.particlePositions array of particle position indexed as
+%   (particle number index,time step index,[u,v,t])
+%   
+
+
+
 brickmanModel.dx=dx;
 brickmanModel.L=L;
 brickmanModel.dy=dy;
@@ -19,31 +42,21 @@ brickmanModel.numberOfTimeSteps=floor(T/dt)+1;
 brickmanModel.numberOfParticles=length(startY);
 brickmanModel.particlePositions=zeros(brickmanModel.numberOfParticles,brickmanModel.numberOfTimeSteps,3);
 brickmanModel.aGridSamples=brickmanAGrid(0,dx,L,0,dy,W,x0,R,u0);
-%TODO use function handle and move duplicate code out side of switch
 switch method
     case 'ODE45'
-        for particle=1:1:brickmanModel.numberOfParticles
-            [To,Yo] = brickmanSolveOde45(0,dt,T,startX,startY(particle),x0,R,u0,options);
-            for timeStep=1:1:brickmanModel.numberOfTimeSteps
-                brickmanModel.particlePositions(particle,timeStep,:)=[Yo(timeStep,:),To(timeStep)];
-            end
-        end
+            h = @brickmanSolveOde45;
     case 'RK4'
-        for particle=1:1:brickmanModel.numberOfParticles
-            [To,Yo] = brickmanSolveRk4(0,dt,T,startX,startY(particle),x0,R,u0,options);
-            for timeStep=1:1:brickmanModel.numberOfTimeSteps
-                brickmanModel.particlePositions(particle,timeStep,:)=[Yo(timeStep,:),To(timeStep)];
-            end
-        end
+            h = @brickmanSolveRk4;
     case 'EULER'
-        for particle=1:1:brickmanModel.numberOfParticles
-            [To,Yo] = brickmanSolveEuler(0,dt,T,startX,startY(particle),x0,R,u0,options);
-            for timeStep=1:1:brickmanModel.numberOfTimeSteps
-                brickmanModel.particlePositions(particle,timeStep,:)=[Yo(timeStep,:),To(timeStep)];
-            end
-        end
+            h = @brickmanSolveEuler;
     otherwise
         disp('Unknown method.')
+end
+for particle=1:1:brickmanModel.numberOfParticles
+    [To,Yo] = h(0,dt,T,startX,startY(particle),x0,R,u0,options);
+    for timeStep=1:1:brickmanModel.numberOfTimeSteps
+        brickmanModel.particlePositions(particle,timeStep,:)=[Yo(timeStep,:),To(timeStep)];
+    end
 end
 
 end
